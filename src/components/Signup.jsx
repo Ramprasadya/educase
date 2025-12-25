6707f6081e0e7364f48f6670013c97f49ecb1d23
Ramprasad yadav <yadavramprasad563@gmail.com>
2025-12-25 22:07:35 +0530

    inital commit

---
+++ src/components/Signup.jsx
@@ -0,0 +1,230 @@
+import React, { useState } from "react";
+
+const Signup = () => {
+  const [login, setLogin] = useState(false);
+  const [signUp, setSignUp] = useState(false);
+  const [loginForm, setLoginForm] = useState({
+    email: "",
+    password: "",
+  });
+  const [signUpForm, setSignUpForm] = useState({
+    fullName: "",
+    phone: "",
+    email: "",
+    password: "",
+    companyName: "",
+    agency: "",
+  });
+  const handleSignupOnChange = (field, value) => {
+    setSignUpForm((prev) => ({
+      ...prev,
+      [field]: value,
+    }));
+  };
+
+  
+  return (
+    <>
+      <div
+        className={`w-full h-screen flex items-end md:justify-center md:items-center p-5 ${
+          login || signUp ? "hidden" : ""
+        }`}
+      >
+        <div className="sm:w-100  flex flex-col gap-y-2">
+          <h1 className="text-2xl font-bold">Welcome to PopX</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta,
+            architecto cupiditate consequuntur a corporis obcaecati.
+          </p>
+          <div className="flex flex-col gap-y-2">
+            <button
+              className="w-full px-3 py-2 bg-blue-600 text-white rounded-md cursor-pointer"
+              onClick={() => setSignUp(true)}
+            >
+              Create Account
+            </button>
+            <button
+              className="w-full px-3 py-2 bg-blue-300 text-black font-bold rounded-md cursor-pointer"
+              onClick={() => setLogin(true)}
+            >
+              Already Registered? Login
+            </button>
+          </div>
+        </div>
+      </div>
+      {/* Login */}
+      {login && (
+        <div className="w-full h-screen flex  md:justify-center md:items-center p-5 ">
+          <div className="sm:w-100  flex flex-col gap-y-2">
+            <h1 className=" text-2xl font-bold">Signin to your PopX account</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+              ipsum inventore fugit totam odit accusantium!
+            </p>
+            <div className="flex flex-col gap-y-3 mt-4">
+              <div className="relative">
+                <label
+                  htmlFor=""
+                  className="absolute -top-2 left-3 bg-[#F9F9F9] px-1 text-sm text-blue-600 font-medium"
+                >
+                  Email Address
+                </label>
+                <input
+                  required
+                  type="email"
+                  value={loginForm.email}
+                  onChange={(e) =>
+                    setLoginForm({ ...loginForm, email: e.target.value })
+                  }
+                  className="w-full px-4 py-2 outline-none border rounded-md"
+                />
+              </div>
+              <div className="relative">
+                <label
+                  htmlFor=""
+                  className="absolute -top-2 left-3 bg-[#F9F9F9] px-1 text-sm text-blue-600 font-medium"
+                >
+                  Password
+                </label>
+                <input
+                  required
+                  type="text"
+                  value={loginForm.password}
+                  onChange={(e) =>
+                    setLoginForm({ ...loginForm, password: e.target.value })
+                  }
+                  className="w-full px-4 py-2 outline-none border rounded-md"
+                />
+              </div>
+            </div>
+            <button className="w-full px-3 py-2 bg-gray-400 text-white font-bold rounded-md cursor-pointer">
+              Login
+            </button>
+          </div>
+        </div>
+      )}
+      {/* Sign Up  */}
+      {signUp && (
+        <div className="w-full h-screen md:flex  md:justify-center md:items-center p-5 ">
+          <div className="sm:w-100  flex flex-col gap-y-2">
+            <h1 className="text-2xl font-bold">Create your <br/> PopX account</h1>
+          <div className="w-full sm:flex sm:flex-col sm:justify-center relative  space-y-4 bg-[#F9F9F9] mt-2">
+           
+            <div className="relative">
+              <label className="absolute -top-2 left-3 bg-[#F9F9F9] px-1 text-sm text-blue-600 font-medium">
+                Full Name
+              </label>
+              <input
+                type="text"
+                required
+                value={signUpForm.fullName}
+                onChange={(e) =>
+                  handleSignupOnChange("fullName", e.target.value)
+                }
+                className="w-full px-4 py-2 border rounded-md outline-none focus:border-purple-500"
+              />
+            </div>
+            <div className="relative">
+              <label className="absolute -top-2 left-3 bg-[#F9F9F9] px-1 text-sm text-blue-600 font-medium">
+                Phone
+              </label>
+              <input
+                type="tel"
+                required
+                value={signUpForm.phone}
+                onChange={(e) => handleSignupOnChange("phone", e.target.value)}
+                className="w-full px-4 py-2 border rounded-md outline-none focus:border-purple-500"
+              />
+            </div>
+            <div className="relative">
+              <label className="absolute -top-2 left-3 bg-[#F9F9F9] px-1 text-sm text-blue-600 font-medium">
+                Email Address
+              </label>
+              <input
+                type="email"
+                required
+                value={signUpForm.email}
+                onChange={(e) => handleSignupOnChange("email", e.target.value)}
+                className="w-full px-4 py-2 border rounded-md outline-none focus:border-purple-500"
+              />
+            </div>
+            <div className="relative">
+              <label className="absolute -top-2 left-3 bg-[#F9F9F9] px-1 text-sm text-blue-600 font-medium">
+                Password
+              </label>
+              <input
+                type="password"
+                required
+                value={signUpForm.password}
+                onChange={(e) =>
+                  handleSignupOnChange("password", e.target.value)
+                }
+                className="w-full px-4 py-2 border rounded-md outline-none focus:border-purple-500"
+              />
+            </div>
+            <div className="relative">
+              <label className="absolute -top-2 left-3 bg-[#F9F9F9] px-1 text-sm text-blue-600 font-medium">
+                Company Name
+              </label>
+              <input
+                type="text"
+                value={signUpForm.companyName}
+                onChange={(e) =>
+                  handleSignupOnChange("companyName", e.target.value)
+                }
+                className="w-full px-4 py-2 border rounded-md outline-none focus:border-purple-500"
+              />
+            </div>
+            {/* Agency - Yes / No */}
+            <div className="">
+              <p className="text-sm text-blue-600 font-medium mb-2">
+                Are you an agency?
+              </p>
+
+              <div className="flex gap-6">
+                <label className="flex items-center gap-2 cursor-pointer">
+                  <input
+                    type="radio"
+                    name="agency"
+                    value="yes"
+                    checked={signUpForm.agency === "yes"}
+                    onChange={(e) =>
+                      handleSignupOnChange("agency", e.target.value)
+                    }
+                    className="accent-blue-600"
+                  />
+                  <span className="text-sm">Yes</span>
+                </label>
+
+                <label className="flex items-center gap-2 cursor-pointer">
+                  <input
+                    type="radio"
+                    name="agency"
+                    value="no"
+                    checked={signUpForm.agency === "no"}
+                    onChange={(e) =>
+                      handleSignupOnChange("agency", e.target.value)
+                    }
+                    className="accent-blue-600"
+                  />
+                  <span className="text-sm">No</span>
+                </label>
+              </div>
+            </div>
+
+            {/* Submit */}
+            <button
+              className="w-full bg-blue-600 text-white py-2 absolute bottom-[-50%] rounded-md
+                   hover:bg-blue-700 transition"
+            >
+              Create Account
+            </button>
+          </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Signup;
